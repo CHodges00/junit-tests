5b99e3748247bf1baa49d69ca70dc423ac4380b5
Cody Hodges <hodges.cody00@gmail.com>
2022-03-14 17:02:53 -0500

    Refactor

--- maven-junit/src/test/java/Student.java
+++ maven-junit/src/test/java/Student.java
@@ -1,100 +1,62 @@
 import java.util.ArrayList;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 
 public class Student {
-
-    long id;
     String name;
-    ArrayList<Integer> Grades = new ArrayList<>();
-
-public Student(){
-}
+    long id;
+    ArrayList<Integer> grades;
 
 
-//          TODO
-//- `id` should be a `long` number used to represent a "unique user" in our application.
-    @Test
-public void isIdEnteredCorrectly() {
-    long id = setId(4567832);
-    if (id == 4567832){
-        assertTrue("ID quals", true);
-    }else {
-        fail("ID doesn't qual");
+
+    public Student(String name, long id) {
+        this.name = name;
+        this.id = id;
+        grades = new ArrayList<>();
     }
-}
 
-@Test
-public void isNameEnteredCorrectly(){
-    String name = setName("tom");
-if (name.equalsIgnoreCase("Tom")){
-    assertTrue("Name quals", true);
-} else {
-    fail("Name doesn't qual");
-}
-}
 
-@Test
-public void doGradesQualify(){
-    ArrayList<Integer> grades = new ArrayList<>();
-    grades.add(100);
-    grades.add(80);
-    grades.add(70);
-    grades.add(80);
-    grades.add(20);
-    Grades.add(100);
-    Grades.add(80);
-    Grades.add(70);
-    Grades.add(80);
-    Grades.add(20);
-    System.out.println(Grades);
-    System.out.println(grades);
-    if (grades == Grades){
-        assertTrue("Grades qual", true);
-    }else {
-        fail("Grades don't qual");
+
+    public String getName() {
+        return name;
     }
-}
 
 
 
-public long setId(long id){
-    this.id = id;
-    return id;
-}
-    // returns the student's id
-    public long getId(){
+    public long getId() {
         return id;
     }
 
-    public String setName(String name){
-    this.name = name;
-        return name;
-    }
-    // returns the student's name
-    public String getName(){
-        return name;
+
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
     }
 
-    // adds the given grade to the grades list
+
+
     public void addGrade(int grade){
-        Grades.add(grade);
+        this.grades.add(grade);
     }
 
-    // returns the list of grades
-    public ArrayList<Integer> getGrades(){
-        return Grades;
-    }
 
-    // returns the average of the students grades
-    public double getGradeAverage(){
-        double average = 0.0;
-        int sum = 0;
-        for (int grade : Grades){
+
+    public double getAverage() {
+        double sum = 0;
+        for (Integer grade : grades) {
             sum += grade;
         }
-        average = (double) sum / Grades.size();
-        return average;
+        return sum / grades.size();
     }
+
+
+    public void updateGrade(int grade, int index){
+        this.grades.set(index, grade);
+    }
+
+
+
+    public void deleteGrade(int index){
+        this.grades.remove(index);
+    }
+
 }
--- maven-junit/src/test/java/StudentTest.java
+++ maven-junit/src/test/java/StudentTest.java
@@ -1,29 +1,55 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 
 public class StudentTest {
 
-    public static void main(String[] args) {
-
-
-
-        Student Tom = new Student();
-        Tom.setId(45678);
-        System.out.println("Tom.getId() = " + Tom.getId());
-        Tom.setName("Tom");
-        System.out.println("Tom.getName() = " + Tom.getName());
-        System.out.println("Tom.getGrades() = " + Tom.getGrades());
-        System.out.println("Tom.getGradeAverage() = " + Tom.getGradeAverage());
-        Tom.addGrade(100);
-        Tom.addGrade(80);
-        Tom.addGrade(70);
-        Tom.addGrade(80);
-        Tom.addGrade(20);
-        System.out.println("Tom.getGrades() = " + Tom.getGrades());
-        System.out.println("Tom.getGradeAverage() = " + Tom.getGradeAverage());
-
-
-
+    @Test
+    public void studentTest() {
+        Student Me = new Student("Cody", 7890765);
+        assertEquals("Cody", Me.getName());
+        assertEquals(7890765, Me.getId());
     }
 
+    @Test
+    public void gradesTest() {
+        Student Me = new Student("Cody", 7890765);
+        Me.addGrade(80);
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(80);
+        assertEquals(Me.getGrades(), grades);
+    }
 
+    @Test
+    public void averageTest() {
+        Student Me = new Student("Cody", 7890765);
+        Me.addGrade(80);
+        Me.addGrade(100);
+        assertEquals(Me.getAverage(), 90, 0);
+    }
+
+    @Test
+    public void updateGradeTest(){
+        Student Me = new Student("Cody", 7890765);
+        Me.addGrade(78);
+        Me.addGrade(65);
+        Me.updateGrade(65, 1);
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(78);
+        grades.add(65);
+        assertEquals(grades, Me.getGrades());
+    }
+
+    @Test
+    public void deleteGradeTest() {
+        Student Me = new Student("Cody", 7890765);
+        Me.addGrade(80);
+        Me.addGrade(90);
+        Me.deleteGrade(1);
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(80);
+        assertEquals(grades, Me.getGrades());
+    }
 }
